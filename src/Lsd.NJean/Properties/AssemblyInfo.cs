67a0d930b410d44176fb5806bd6c8ef71c023a63
diadistis <diadistis@7a940548-1eb9-11de-bf1e-7b5f7763e386>
2009-04-02 23:14:25 +0000

    Initial Solution

---
+++ src/Lsd.NJean/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿
+#region Imports
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("NJean")]
+[assembly: AssemblyDescription("NJean Template Engine")]
+[assembly: AssemblyCompany("Liquid Software Development")]
+[assembly: AssemblyProduct("NJean")]
+[assembly: AssemblyCopyright("Copyright © 2009")]
+[assembly: ComVisible(false)]
+[assembly: Guid("946d6136-1173-48bd-a19d-a6a37e2e6c10")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
