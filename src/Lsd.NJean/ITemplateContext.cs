58e29eabae3a239dc3605cd17dc371bed7f492b2
diadistis <diadistis@7a940548-1eb9-11de-bf1e-7b5f7763e386>
2009-04-05 00:47:56 +0000

    Added build scripts and tools(NAnt, FxCop, StyleCop) and ITemplate, ITemplateContext, TemplateContext

---
+++ src/Lsd.NJean/ITemplateContext.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ITemplateContext.cs" company="Liquid Software Development">
+// Copyright 2008-2009 Liquid Software Development - http://www.liquidsd.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+namespace Lsd.NJean
+{
+    #region Imports
+
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    #endregion
+
+    [SuppressMessage
+        ("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", 
+        Justification = "Context is a much better suffix than Dictionary.")]
+    public interface ITemplateContext : IDictionary<string, object>
+    {
+    }
+}
